0f7b77959712c276572987ce428ff7acdbcc7193
Leonardo <leonardo.novaro.ln@gmail.com>
2021-06-06 12:54:37 -0400

    Responsive functionality of the header when displaying the menu

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Header = () => {
-    return ( 
-        <header className="header">
-            <a href="#" className="header__logo">Portfolio</a>
-            <div className="toggle-menu"></div>
-            <div className="header__menu">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Work</a>
-                <a href="#">Contact</a>
-            </div>
-        </header>
-    );
-}
+	const [showmenu, setShowMenu] = useState(false);
+
+	typeof window !== "undefined" &&
+		window.addEventListener("scroll", function () {
+			document
+				.querySelector("#header")
+				.classList.toggle("active", window.scrollY > 0);
+		});
+
+	return (
+		<header className="header" id="header">
+			<a href="#" className="header__logo">
+				Portfolio
+			</a>
+			<div
+				className={showmenu ? "toggle-menu active" : "toggle-menu"}
+				onClick={() => setShowMenu(!showmenu)}
+			></div>
+			<div
+				className={showmenu ? "header__menu active" : "header__menu"}
+				onClick={() => setShowMenu(!showmenu)}
+			>
+				<a href="#">Home</a>
+				<a href="#">About</a>
+				<a href="#">Work</a>
+				<a href="#">Contact</a>
+			</div>
+		</header>
+	);
+};
 export default Header;
